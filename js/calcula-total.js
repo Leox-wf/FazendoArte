d7f2a2c6f3349f3412e47b37e82a6268da0994b5
Leox-wf <78910610+Leox-wf@users.noreply.github.com>
2024-02-15 22:35:59 -0300

    subindo o projetinho

---
+++ js/calcula-total.js
@@ -0,0 +1,35 @@
+//Captura todos os clientes que fizeram encomendas
+var clientes = document.querySelectorAll(".cliente");
+
+//Passa por cada encomenda, calculando o valor total
+for (var count=0; count < clientes.length; count++){
+    //captura a quantidade encomenda
+    var qtde = clientes[count].querySelector(".info-qtde").textContent;
+
+    //captura o valor unitario do produto
+    var unitario = clientes[count].querySelector(".info-valor").textContent;
+
+    //valida a quantidade
+    if(qtde<1 || isNaN(qtde)){
+        //quantidade ok, avisa o usuario
+        clientes[count].querySelector(".info-qtde").textContent = "qtde invalida!";
+        //clientes[count].style.color="red";
+        clientes[count].style.backgroundColor="red";
+    }else{
+        //Quantidade ok, prossegue
+        //calcula o valor total da encomenda
+        clientes[count].querySelector(".info-total").textContent = calculaTotal(qtde,unitario);
+    }
+
+    //calcula o valor totaç da encomenda
+    //1º teste clientes[count].querySelector(".info-total").textContent = qtde * unitario;
+    //2º teste clientes[count].querySelector(".info-total").textContent = calculaTotal(qtde,unitario);
+
+}
+
+//Função para calcular o valor total da encomenda
+function calculaTotal(qtde,unitario){
+    var total = 0;
+    total=qtde*unitario;
+    return total;
+}
